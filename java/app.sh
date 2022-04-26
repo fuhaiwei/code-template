96df7d29754fc7f44efbf0fe107376fb8ae6d87d
fuhaiwei <fuhaiwei@126.com>
2022-04-26 23:40:22 +0800

    update app.sh

--- java/app.sh
+++ java/app.sh
@@ -1,147 +1,153 @@
 #!/usr/bin/env bash
 
-boot_run_with_nohup() {
-    echo "bash ./mvnw spring-boot:run >$LogFile 2>&1 &"
-    nohup bash ./mvnw spring-boot:run >"$LogFile" 2>&1 &
+# 环境准备
+DirName=$(dirname "$0")
+AppHome=$(realpath "$DirName")
+RunHome="$AppHome"/var
+LogFile="$RunHome"/app.log
+StdFile="$RunHome"/std.log
+PidFile="$RunHome"/run.pid
+Param1="${1:-help}"
+Param2="$2"
 
-    echo $! >"$PidFile"
-    echo "The server is starting : $!"
-}
+mkdir -p "$RunHome"
+cd "$AppHome" || exit
 
-java_jar_with_nohup() {
-    echo "java -jar $JarFile --spring.profiles.active=pro >>$LogFile 2>&1 &"
-    nohup java -jar "$JarFile" --spring.profiles.active=pro >>"$LogFile" 2>&1 &
-
-    echo $! >"$PidFile"
-    echo "The server is starting : $!"
-}
-
-package_if_unpacked() {
-    if [[ -d $JarPath ]]; then
-        JarFile=$(find "$JarPath" -name '*.jar' | tail -1)
-    fi
-    if [[ -z $JarFile ]]; then
-        maven_clean_package
-    else
-        rm -f "$LogFile"
-    fi
-}
-
-maven_clean_package() {
-    echo "bash ./mvnw clean package >$LogFile 2>&1"
-    bash ./mvnw clean package >"$LogFile" 2>&1
-}
-
-exit_if_build_faild() {
-    JarFile=$(find target -name '*.jar' | tail -1)
-
-    if [[ -z $JarFile ]]; then
-        echo "An error occurred during packaging"
-        exit 1
-    fi
-}
-
-test_is_app_running() {
-    if [[ -f $PidFile ]]; then
+# 函数定义
+test_run() {
+    if [[ -f "$PidFile" && -n $(cat "$PidFile") ]]; then
         PidText=$(cat "$PidFile")
-        if [[ -n $PidText ]]; then
-            PsCount=$(ps -p "$PidText" | wc -l)
-            if [[ $PsCount -eq 2 ]]; then
-                Running="true"
-            else
-                rm -f "$PidFile"
-            fi
+        if [[ $(ps -p "$PidText" | wc -l) -eq 2 ]]; then
+            Running="true"
         fi
     fi
 }
 
-exit_if_app_running() {
-    if [[ $Running == "true" ]]; then
-        echo "The server cannot be started, it has already started : $PidText"
-        exit 2
+boot_run() {
+    echo mvn clean spring-boot:run -Dspring-boot.run.profiles=dev
+    nohup bash ./mvnw clean spring-boot:run -Dspring-boot.run.profiles=dev >"$StdFile" 2>&1 &
+    echo $! >"$PidFile"
+    echo "The server is starting : $!"
+}
+
+build_if() {
+    JarNums=$(find target -name '*.jar' -print | wc -l)
+    if [[ "$1" == "-f" || "$JarNums" -eq 0 ]]; then
+        echo mvn clean package
+        bash ./mvnw clean package
     fi
 }
 
-kill_if_app_running() {
-    if [[ $Running == "true" ]]; then
-        echo "The server is stopping : $PidText"
-        kill "$PidText"
-        rm "$PidFile"
+java_jar() {
+    JarFile=$(find target -name '*.jar' -print | tail -1)
+    if [[ -f "$JarFile" ]]; then
+        echo java -Xms64m -Xmx256m -jar "$JarFile" --spring.profiles.active=prod
+        nohup java -Xms64m -Xmx256m -jar "$JarFile" --spring.profiles.active=prod >"$StdFile" 2>&1 &
+        echo $! >"$PidFile"
+        echo "The server is starting : $!"
+    else
+        echo "Could not start, jar file not found"
+    fi
+}
+
+kill_app() {
+    if [[ "$1" == "-f" ]]; then
+        echo kill -9 "$PidText"
+        kill -9 "$PidText"
         sleep 1
+    else
+        echo kill "$PidText"
+        kill "$PidText"
+        while /bin/true; do
+            sleep 1
+            [[ $(ps -p "$PidText" | wc -l) -lt 2 ]] && break
+        done
+    fi
+    rm "$PidFile"
+    echo "The server is stopped"
+}
+
+try_stop() {
+    if [[ "$Running" == "true" ]]; then
+        if [[ "$1" == "-f" ]]; then
+            kill_app
+            sleep 1
+        else
+            echo "The server cannot be started, it has already started : $PidText"
+            exit 0
+        fi
     fi
 }
 
-# 环境准备
-DirName=$(dirname "$0")
-HomeDir=$(realpath "$DirName")
-JarPath="$HomeDir/target"
-ExecDir=$HomeDir/var
-LogFile="$ExecDir/std.log"
-PidFile="$ExecDir/run.pid"
-ParamCmd=${1:-help}
+test_run
 
-cd "$HomeDir" || exit
-mkdir -p "$ExecDir"
-test_is_app_running
-
-case $ParamCmd in
-st | start) [[ $2 == "-f" ]] && ParamCmd="restart" ;;
-esac
-
-# 程序开始
-case $ParamCmd in
-d | dt | dev)
-    kill_if_app_running
-
-    boot_run_with_nohup
-
-    tail -f "$LogFile"
+# 参数解析
+case "$Param1" in
+d | dd | dev)
+    try_stop -f
+    boot_run
+    sleep 1
+    tail -f "$StdFile"
     ;;
 st | start)
-    exit_if_app_running
-
-    package_if_unpacked
-
-    exit_if_build_faild
-
-    java_jar_with_nohup
+    build_if "$Param2"
+    try_stop "$Param2"
+    java_jar
+    ;;
+qt | stop)
+    if [[ "$Running" == "true" ]]; then
+        kill_app "$Param2"
+    else
+        echo "The server is not running"
+    fi
     ;;
 rt | restart)
-    maven_clean_package
-
-    exit_if_build_faild
-
-    kill_if_app_running
-
-    java_jar_with_nohup
+    build_if -f
+    try_stop -f
+    java_jar
     ;;
 vt | status)
-    if [[ $Running == "true" ]]; then
+    if [[ "$Running" == "true" ]]; then
         echo "The server is running : $PidText"
     else
         echo "The server is not running"
     fi
     ;;
-qt | stop)
-    if [[ $Running == "true" ]]; then
-        kill_if_app_running
-    else
-        echo "The server is not running"
-    fi
-    ;;
-sl | log)
-    if [[ $2 == "-f" ]]; then
-        tail -f "$LogFile"
-    else
+log)
+    if [[ "$Param2" == "-a" ]]; then
         less "$LogFile"
+    else
+        tail -f "$LogFile"
     fi
     ;;
-help | *)
-    echo "usage: bash app.sh [d|dt|dev]"
+std)
+    if [[ "$Param2" == "-a" ]]; then
+        less "$StdFile"
+    else
+        tail -f "$StdFile"
+    fi
+    ;;
+fed)
+    echo git fetch origin develop
+    git fetch origin develop
+    echo git reset --hard origin/develop
+    git reset --hard origin/develop
+    ;;
+fem)
+    echo git fetch origin master
+    git fetch origin master
+    echo git reset --hard origin/master
+    git reset --hard origin/master
+    ;;
+*)
+    echo "usage: bash app.sh [d|dd|dev]"
     echo "usage: bash app.sh [st|start] [-f]"
+    echo "usage: bash app.sh [qt|stop] [-f]"
     echo "usage: bash app.sh [rt|restart]"
     echo "usage: bash app.sh [vt|status]"
-    echo "usage: bash app.sh [qt|stop]"
-    echo "usage: bash app.sh [sl|log] [-f]"
+    echo "usage: bash app.sh log [-a]"
+    echo "usage: bash app.sh std [-a]"
+    echo "usage: bash app.sh fed"
+    echo "usage: bash app.sh fem"
     ;;
 esac
